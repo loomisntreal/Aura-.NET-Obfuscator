d167c9e11879a6df0ffc16e8cac0980632a3f6cf
loom <71902422+loomisntreal@users.noreply.github.com>
2020-12-01 23:34:59 -0500

    Add files via upload

---
+++ Aura/Aura/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyTitle("Aura")]
+[assembly: AssemblyDescription("https://github.com/Sato-Isolated/Aura")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Aura Services")]
+[assembly: AssemblyProduct("Aura")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(true)]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: Guid("20180FD2-96D4-478B-BA67-C72FB46CCFD4")]
